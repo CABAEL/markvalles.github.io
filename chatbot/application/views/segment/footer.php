33aa4cc03a4deb7c9e1280e38158511d0211536e
Mark Valles <vallesmark15@gmail.com>
2020-12-16 11:00:50 +0800

    sample

---
+++ chatbot/application/views/segment/footer.php
@@ -0,0 +1,19 @@
+<script src="<?=JSPATH?>jquery-3.1.0.min.js"></script>
+<script src="<?=JSPATH?>bootstrap.min.js"></script>
+<script src="<?=JSPATH?>bootstrap-datepicker.js"></script>
+<script src="<?=JSPATH?>jquery.slimscroll.min.js"></script>
+<script src="<?=JSPATH?>jquery.uniform.standalone.js"></script>
+<script src="<?=JSPATH?>switchery.min.js"></script>
+<script src="<?=JSPATH?>d3.min.js"></script>
+<script src="<?=JSPATH?>nv.d3.min.js"></script>
+<script src="<?=JSPATH?>jquery.flot.min.js"></script>
+<script src="<?=JSPATH?>jquery.flot.time.min.js"></script>
+<script src="<?=JSPATH?>jquery.flot.symbol.min.js"></script>
+<script src="<?=JSPATH?>jquery.flot.resize.min.js"></script>
+<script src="<?=JSPATH?>jquery.flot.tooltip.min.js"></script>
+<script src="<?=JSPATH?>jquery.flot.pie.min.js"></script>
+
+<script src="<?=JSPATH?>jsfile.js"></script>
+<script src="<?=JSPATH?>dashboard.js"></script>
+</body>
+</html>
