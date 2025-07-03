eab98f2925abc6d4f019af744e98609ff95101b3
ALPHA\USER <vallesmark15@gmail.com>
2025-07-03 09:19:48 +0800

    test

--- v2/vite.config.js
+++ v2/vite.config.js
@@ -4,19 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'markvalles.github.io/public/',
+  base: '/markvalles.github.io/public/',
     build: {
     outDir: '../public',
-    emptyOutDir: true,
-    // rollupOptions: {
-    //   output: {
-    //     // Put assets inside /public
-    //     entryFileNames: '[name].[hash].js',       
-    //     chunkFileNames: '[name].[hash].js',
-    //     assetFileNames: '[name].[hash][extname]',
+    emptyOutDir: false,
+    rollupOptions: {
+      output: {
+        // Put assets inside /public
+        // entryFileNames: '[name].[hash].js',       
+        // chunkFileNames: '[name].[hash].js',
+        // assetFileNames: '../[name].[hash][extname]',
 
-    //   },
-    // },
+      },
+    },
 
   }
 })
